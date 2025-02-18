0638ef2b2e3599006c1bd2b4157a0456b3494da7
🦈 Carlos Solórzano Cerdas <csolorzano@istmocenter.com>
2025-02-18 07:42:13 -0600

    improved zen focus

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,20 @@
+-- lazy.nvim
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      styles = {
+        zen = {
+          --     minimal = false,
+          backdrop = { transparent = true, blend = 5 },
+          --     wo = {
+          --       winhighlight = "NormalFloat:Normal",
+          --     },
+          --     w = {
+          --       snacks_main = true,
+          --     },
+        },
+      },
+    },
+  },
+}
